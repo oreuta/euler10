2e18411efa2103546755fab6248da4ce79229ce3
O. Reuta <reutaov@gmail.com>
2016-12-22 12:52:34 +0200

    Checked for 2000000 => 142913828922

--- app.go
+++ app.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	fmt.Println("Hello euler 10!")
-	n := 100
+	n := 2000000
 	primes, sum := erat(n)
 	fmt.Printf("act: %v\n", primes)
 	fmt.Printf("exp: [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97]")
