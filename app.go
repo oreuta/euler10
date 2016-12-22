3a9edd340de3a66034e30cbe2bc8352483e03458
O. Reuta <reutaov@gmail.com>
2016-12-22 11:44:14 +0200

    the simplest erat

--- app.go
+++ app.go
@@ -7,9 +7,17 @@ import (
 )
 
 func main() {
-	n, err := conf.Getconf(10)
+	n, err := conf.Getconf(100)
 	if err != nil {
 		log.Fatalf("Bad config: %v", err)
 	}
-	fmt.Printf("Hello euler %v config: %d\n", erat(n), n)
+	fmt.Println("Hello euler 10!")
+	primes := erat(n)
+	fmt.Printf("act: %v\n", primes)
+	fmt.Printf("exp: [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97]")
+	fmt.Println()
+}
+
+func print_primes(n []int) {
+	fmt.Printf("%v\n", n)
 }
--- erat.go
+++ erat.go
@@ -1,6 +1,37 @@
 package main
 
-func erat(n int) []bool {
-	var s [2000000]bool
-	return s[2:n]
+//Sieve of Eratosthenes - The Simplest Algorithm
+func erat(n int) []int {
+
+	type Sieve struct {
+		i int  // a number
+		f bool // false = prime
+	}
+
+	// Init []Sieve
+	s := make([]Sieve, n)
+	for i := 0; i < n; i++ {
+		s[i].i = i
+	}
+	s[0].f, s[1].f = true, true // 0 and 1 are not primes
+
+	i := 2      // first prime
+	for i < n { // main loop
+		for j := i * i; j < n; j += i {
+			s[j].f = true
+		}
+		i++
+		for i < n && s[i].f { // find next prime
+			i++
+		}
+	}
+
+	// Pick up primes from []Sieve to []int
+	p := make([]int, 0)
+	for i := 0; i < n; i++ {
+		if !s[i].f {
+			p = append(p, s[i].i)
+		}
+	}
+	return p
 }
