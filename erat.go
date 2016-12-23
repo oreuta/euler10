6ee913359c39e82bfd53ede61c84fb2d1fae6069
O. Reuta <reutaov@gmail.com>
2016-12-23 19:02:30 +0200

    Structured for parallel processing

--- erat.go
+++ erat.go
@@ -7,21 +7,28 @@ func erat(n int, list bool) (int, []int) {
 	s := make([]bool, n)
 	s[0], s[1] = true, true // 0 and 1 are composite
 
-	i := 2      // first prime
-	sum := i    // sum of primes
-	pnum := 0   // number of primes
+	i := 2    // first prime
+	sum := i  // sum of primes
+	pnum := 0 // number of primes
+
 	for i < n { // sieve main loop
+
+		// go routine starts here ----
 		for j := i * i; j < n; j += i {
 			s[j] = true
 		}
+		// ----------------------------
+
+		//find next prime, count it, update sum
 		i++
-		for i < n && s[i] { // go to the next prime
+		for i < n && s[i] {
 			i++
 		}
-		sum += i
 		pnum++
+		sum += i
 	}
 
+	// prepare list of primes if needed
 	if list {
 		return sum, make_prime_list(s, pnum)
 	}
