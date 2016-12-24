e017dbe479d11db4c2788a9e5317bf1670572281
O. Reuta <reutaov@gmail.com>
2016-12-24 07:49:44 +0200

    Parallel 2,3,5,7,11,13

--- app.go
+++ app.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello euler 10!")
 	n := 2000000
-	sum, primes := erat(n, false)
+	ts := time.Now()
+	sum, _ := erat(n, false)
+	te := time.Now()
 	//
 	//sum is 142913828922 (for n = 2000000)
 	//You are the 228112th person to have solved this problem.
 	//
-	fmt.Printf("act: %v\n", primes)
-	fmt.Printf("exp: [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97]")
-	fmt.Printf("\nSum: %v\n", sum)
+	//	fmt.Printf("act: %v\n", primes)
+	//	fmt.Printf("exp: [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97]")
+	fmt.Printf("Sum: %v\n", sum)
+	fmt.Printf("Dur: %v\n", te.Nanosecond()-ts.Nanosecond())
+
+	ts = time.Now()
+	sump, _ := eratp(n, false)
+	te = time.Now()
+
+	//	fmt.Printf("act: %v\n", primesp)
+	fmt.Printf("\nSum: %v\n", sump)
+	fmt.Printf("Dur: %v\n", te.Nanosecond()-ts.Nanosecond())
+
 }
 
 func print_primes(n []int) {
--- erat.go
+++ erat.go
@@ -1,6 +1,72 @@
 package main
 
 //Sieve of Eratosthenes - The Simplest Algorithm
+//Parallel version 1 (fixed g-rout number)
+func eratp(n int, list bool) (int, []int) {
+
+	// All numbers are set to be prime (false)
+	s := make([]bool, n)
+	s[0], s[1] = true, true // 0 and 1 are composite
+
+	i := 0    // first prime
+	sum := 0  // sum of primes
+	pnum := 0 // number of primes
+
+	ch := make(chan int, 4)
+
+	go rmcomp(s, 2, ch)
+	go rmcomp(s, 3, ch)
+	go rmcomp(s, 5, ch)
+	go rmcomp(s, 7, ch)
+	go rmcomp(s, 11, ch)
+	go rmcomp(s, 13, ch)
+
+	sum = 2 + 3 + 5 + 7 + 11 + 13
+	pnum = 6
+
+	<-ch
+	<-ch
+	<-ch
+	<-ch
+	<-ch
+	<-ch
+
+	i = 13
+
+	for i < n { // sieve main loop
+
+		// go routine starts here ----
+		for j := i * i; j < n; j += i {
+			s[j] = true
+		}
+		// ----------------------------
+
+		//find next prime, count it, update sum
+		i++
+		for i < n && s[i] {
+			i++
+		}
+		pnum++
+		sum += i
+	}
+
+	// prepare list of primes if needed
+	if list {
+		return sum, make_prime_list(s, pnum)
+	}
+
+	return sum, nil
+
+}
+
+func rmcomp(s []bool, i int, c chan int) {
+	for j := i * i; j < len(s); j += i {
+		s[j] = true
+	}
+	c <- i
+}
+
+//Sieve of Eratosthenes - The Simplest Algorithm
 func erat(n int, list bool) (int, []int) {
 
 	// All numbers are set to be prime (false)
